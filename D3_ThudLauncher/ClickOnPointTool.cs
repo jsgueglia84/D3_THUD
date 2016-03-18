a6c9a8d024689cc2a6b62c415dcfbf317a1a0fca
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2016-03-18 14:30:40 -0400

    Version 1.0

---
+++ D3_ThudLauncher/ClickOnPointTool.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace D3_ThudLauncher
+{
+    public class ClickOnPointTool
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] Input[] pInputs, int cbSize);
+
+        internal struct Input
+        {
+            public uint Type;
+            public Mousekeybdhardwareinput Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct Mousekeybdhardwareinput
+        {
+            [FieldOffset(0)]
+            public MouseInput Mouse;
+        }
+
+        internal struct MouseInput
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+
+        public static void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
+        {
+            var oldPos = Cursor.Position;
+
+            //get screen coordinates
+            ClientToScreen(wndHandle, ref clientPoint);
+
+            //set cursor on coords, and press mouse
+            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
+
+            var inputMouseDown = new Input {Type = 0};
+            //input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; //left button down
+
+            var inputMouseUp = new Input {Type = 0};
+            //input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; //left button up
+
+            var inputs = new[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+
+            //return mouse 
+            Cursor.Position = oldPos;
+        }
+
+    }
+}
---
+++ D3_ThudLauncher/Launcher.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace D3_ThudLauncher
+{
+    internal class Launcher
+    {
+        private static void Main(string[] args)
+        {
+            //Default path for my PC + allow anyone to cmd the sh!t out of these
+            //Param 1 = THUD Path (including exe)
+            //Param 2 = D3 Path (including exe)
+            var thudPath = args.Length > 0 ? args[0] : @"D:\THUD\THUD.exe";
+            var d3Path = args.Length > 1 ? args[1] : @"C:\Program Files\Diablo III\Diablo III.exe";
+
+            //Make sure we won't try to launch something that does not exists
+            if (!File.Exists(thudPath)) throw new FileNotFoundException(thudPath);
+            if(!File.Exists(d3Path)) throw new FileNotFoundException(d3Path);
+
+            //Starts Battle.Net
+            var bnetHandle = StartBNet(d3Path);
+            //Starts Diablo 3 from Battle.Net (click at X,Y position on standard windows size)
+            StartDiablo(bnetHandle);
+            //Stops Battle.Net from being an arse
+            KillBNet();
+            //Starts THUD
+            StartThud(thudPath);
+        }
+
+        private static void StartThud(string thudPath)
+        {
+            //Give 5 second to start D3, even if it takes more like 10
+            Thread.Sleep(5000);
+            Process.Start(thudPath);
+        }
+
+        private static IntPtr StartBNet(string d3Path)
+        {
+            Process.Start(d3Path);
+            return BringToFront("Battle.net");
+        }
+
+        private static void StartDiablo(IntPtr handle)
+        {
+            //Makes sure D3 starts
+            while (Process.GetProcessesByName("Diablo III").Length < 1)
+            {
+                //Click at coords 290, 677 relatively to the Window's position
+                ClickOnPointTool.ClickOnPoint(handle, new Point(290, 677));
+                //Avoid overusing the CPU if we have to click alot
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void KillBNet()
+        {
+            //Get Battle.net process
+            var bnetProcess = Process.GetProcessesByName("Battle.net")[0];
+            //Kill it
+            bnetProcess.Kill();
+        }
+
+        public static IntPtr BringToFront(string title)
+        {
+            // Get a handle to the Calculator application.
+            var handle = FindWindow(null, title);
+
+            //Wait until the app starts and can be shown
+            while (handle == IntPtr.Zero)
+            {
+                //Sleeps one sec to not over-charge the CPU
+                Thread.Sleep(1000);
+                //Reset the handle
+                handle = FindWindow(null, title);
+            }
+            
+
+            // Make Calculator the foreground application
+            SetForegroundWindow(handle);
+            return handle;
+        }
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
