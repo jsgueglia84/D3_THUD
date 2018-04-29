0dc50515e8f1ea26bf3f56666b482c85d3ed50d0
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2018-04-29 10:22:15 -0400

    Ensure printing of error when something happens

--- D3_ThudLauncher/Launcher.cs
+++ D3_ThudLauncher/Launcher.cs
@@ -7,6 +7,7 @@ using System.Security;
 using System.Security.AccessControl;
 using System.Security.Principal;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace D3_ThudLauncher
 {
@@ -16,47 +17,55 @@ namespace D3_ThudLauncher
         private static bool IsElevated => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
         private static void Main(string[] args)
         {
-            if(!IsElevated)throw new PrivilegeNotHeldException("You must run this as Admin");
-            //Default path for my PC + allow anyone to cmd the sh!t out of these
-            //Param 1 = THUD Path (including exe)
-            //Param 2 = D3 Path (including exe)
-            //Param 3 = User name to run Diablo
-            //Param 4 = Password for the user that'll run Diablo
-            var thudPath = args.Length > 0 ? args[0] : @"D:\THUD\THUD.exe";
-            var d3Path = args.Length > 1 ? args[1] : @"D:\Program Files (x86)\Diablo III\Diablo III.exe";
-            var spwD3 = new SecureString();
-            var pw = args.Length > 3 ? args[3] : "diablo";
-            foreach (var c in pw)
+            try
             {
-                spwD3.AppendChar(c);
-            }
-
-            var workingDirectory = new FileInfo(d3Path).DirectoryName;
-            if (workingDirectory != null)
-            {
-                var d3StartInfo = new ProcessStartInfo
+                if (!IsElevated) throw new PrivilegeNotHeldException("You must run this as Admin");
+                //Default path for my PC + allow anyone to cmd the sh!t out of these
+                //Param 1 = THUD Path (including exe)
+                //Param 2 = D3 Path (including exe)
+                //Param 3 = User name to run Diablo
+                //Param 4 = Password for the user that'll run Diablo
+                var thudPath = args.Length > 0 ? args[0] : @"D:\THUD\THUD.exe";
+                var d3Path = args.Length > 1 ? args[1] : @"D:\Program Files (x86)\Diablo III\Diablo III.exe";
+                var spwD3 = new SecureString();
+                var pw = args.Length > 3 ? args[3] : "diablo";
+                foreach (var c in pw)
                 {
-                    UserName = args.Length > 2 ? args[2] : "diablo",
-                    Password = spwD3,
-                    FileName = d3Path,
-                    WorkingDirectory = workingDirectory,
-                    UseShellExecute = false,
-                    LoadUserProfile = true
-                };
+                    spwD3.AppendChar(c);
+                }
 
-                //Make sure we won't try to launch something that does not exists
-                if (!File.Exists(thudPath)) throw new FileNotFoundException(thudPath);
-                if(!File.Exists(d3Path)) throw new FileNotFoundException(d3Path);
+                var workingDirectory = new FileInfo(d3Path).DirectoryName;
+                if (workingDirectory != null)
+                {
+                    var d3StartInfo = new ProcessStartInfo
+                    {
+                        UserName = args.Length > 2 ? args[2] : "diablo",
+                        Password = spwD3,
+                        FileName = d3Path,
+                        WorkingDirectory = workingDirectory,
+                        UseShellExecute = false,
+                        LoadUserProfile = true
+                    };
 
-                //Starts Battle.Net
-                var bnetHandle = StartBNet(d3StartInfo);
-                //Starts Diablo 3 from Battle.Net (click at X,Y position on standard windows size)
-                StartDiablo(bnetHandle);
+                    //Make sure we won't try to launch something that does not exists
+                    if (!File.Exists(thudPath)) throw new FileNotFoundException(thudPath);
+                    if (!File.Exists(d3Path)) throw new FileNotFoundException(d3Path);
+
+                    //Starts Battle.Net
+                    var bnetHandle = StartBNet(d3StartInfo);
+                    //Starts Diablo 3 from Battle.Net (click at X,Y position on standard windows size)
+                    StartDiablo(bnetHandle);
+                }
+
+                //Stops Battle.Net from being an arse
+                KillBNet();
+                //Starts THUD
+                StartThud(thudPath);
             }
-            //Stops Battle.Net from being an arse
-            KillBNet();
-            //Starts THUD
-            StartThud(thudPath);
+            catch (Exception e)
+            {
+                WarnAndThrow(e);
+            }
         }
 
         private static void StartThud(string thudPath)
@@ -103,6 +112,12 @@ namespace D3_ThudLauncher
             var bnetProcess = bnetProcesses[0];
             //Kill it
             bnetProcess.Kill();
+        }
+
+        private static void WarnAndThrow(Exception e)
+        {
+            MessageBox.Show(e.Message, e.GetType().Name);
+            throw e;
         }
 
         public static IntPtr BringToFront(string title)
